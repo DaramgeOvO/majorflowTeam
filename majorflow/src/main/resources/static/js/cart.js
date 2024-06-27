96b41ba816e750e38f0da06ebe82328620a91284
DaramgeOvO <lovely21144@gmail.com>
2024-06-27 15:12:28 +0900

    공지사항쪽 html css

--- majorflow/src/main/resources/static/js/cart.js
+++ majorflow/src/main/resources/static/js/cart.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   let userId;
   let itemToDelete;
-  let itemIndexToDelete;
+  let cartItems = []; // 전역 변수로 선언
+
   sessionCurrent();
 
-  // 세션을 통해 현재 사용자를 확인하는 함수
   function sessionCurrent() {
     axios
       .get("http://localhost:8080/user/current", { withCredentials: true })
@@ -19,8 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
           document.querySelector(".menuLoginBtn").classList.add("hidden");
           document.querySelector(".menuLogoutBtn").classList.remove("hidden");
 
-          let cartItems = JSON.parse(localStorage.getItem(userId));
-          fetchAndRenderCart(cartItems, userId);
+          displayCartItems(userId);
         } else {
           document.querySelector(".menuLogoutBtn").classList.add("hidden");
           document.querySelector(".menuLoginBtn").classList.remove("hidden");
@@ -31,120 +30,187 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  // 장바구니 데이터를 가져와 화면에 출력하는 함수
-  function fetchAndRenderCart(item, userId) {
-    const cartItemsContainer = document.getElementById("cartItemsContainer");
+  function displayCartItems(userId) {
+    axios
+      .get("http://localhost:8080/cart/user/" + userId)
+      .then((response) => {
+        console.log("데이터: ", response);
+        cartItems = response.data;
+        const cartItemsContainer = document.querySelector(
+          ".cartItemsContainer"
+        );
 
-    // 기존 아이템 제거
-    cartItemsContainer.innerHTML = "";
+        // 기존의 장바구니 아이템을 삭제
+        cartItemsContainer.innerHTML = "";
 
-    // 장바구니 아이템들을 UI에 추가
-    item.forEach((data, index) => {
-      const cartBox1 = document.createElement("div");
-      cartBox1.classList.add("cartBox1");
+        // 장바구니 아이템들을 UI에 추가
+        cartItems.forEach((item) => {
+          console.log(item);
+          const cartBox1 = document.createElement("div");
+          cartBox1.classList.add("cartBox1");
+          cartBox1.setAttribute("data-item-id", item.id);
 
-      const cartBox2 = document.createElement("div");
-      cartBox2.classList.add("cartBox2");
-      cartBox2.textContent = data.lectureName;
+          const cartBox2 = document.createElement("div");
+          cartBox2.classList.add("cartBox2");
+          cartBox2.textContent = item.lecture.lectureName;
 
-      const cartBoxLine = document.createElement("div");
-      cartBoxLine.classList.add("cartBoxLine");
+          const cartBoxLine = document.createElement("div");
+          cartBoxLine.classList.add("cartBoxLine");
 
-      const cartBox3 = document.createElement("div");
-      cartBox3.classList.add("cartBox3");
-      cartBox3.textContent = data.teacherName;
+          const cartBox3 = document.createElement("div");
+          cartBox3.classList.add("cartBox3");
+          cartBox3.textContent = item.lecture.teacher.teacherName;
 
-      const cartBox4 = document.createElement("div");
-      cartBox4.classList.add("cartBox3");
-      cartBox4.textContent = data.type;
+          const cartBox4 = document.createElement("div");
+          cartBox4.classList.add("cartBox3");
+          cartBox4.textContent = item.lecture.lectureClass;
 
-      const cartBox5 = document.createElement("div");
-      cartBox5.classList.add("cartBox3");
-      cartBox5.textContent = data.price + "원";
+          const cartBox5 = document.createElement("div");
+          cartBox5.classList.add("cartBox3");
+          cartBox5.textContent = item.lecture.price + "원";
 
-      const cartBoxRemove = document.createElement("div");
-      cartBoxRemove.classList.add("cartBoxRemove");
-      cartBoxRemove.textContent = "삭제";
-      cartBoxRemove.addEventListener("click", () => {
-        itemToDelete = item;
-        itemIndexToDelete = index;
-        openModal("장바구니에서 삭제하시겠습니까?");
+          const cartBoxRemove = document.createElement("div");
+          cartBoxRemove.classList.add("cartBoxRemove");
+          cartBoxRemove.textContent = "삭제";
+          cartBoxRemove.addEventListener("click", () => {
+            itemToDelete = item;
+            openModal("장바구니에서 삭제하시겠습니까?");
+          });
+
+          cartItemsContainer.appendChild(cartBox1);
+          cartBox1.appendChild(cartBox2);
+          cartBox1.appendChild(cartBoxLine);
+          cartBox1.appendChild(cartBox3);
+          cartBox1.appendChild(cartBox4);
+          cartBox1.appendChild(cartBox5);
+          cartBox1.appendChild(cartBoxRemove);
+        });
+
+        updateTotalPrice(cartItems);
+
+        document.querySelector(".cartBox6").addEventListener("click", () => {
+          openModal("구매하시겠습니까?");
+        });
+
+        document
+          .getElementById("alertConfirm")
+          .addEventListener("click", handleAlertConfirm);
+      })
+      .catch((error) => {
+        console.log("에러발생: ", error);
       });
 
-      cartItemsContainer.appendChild(cartBox1);
-      cartBox1.appendChild(cartBox2);
-      cartBox1.appendChild(cartBoxLine);
-      cartBox1.appendChild(cartBox3);
-      cartBox1.appendChild(cartBox4);
-      cartBox1.appendChild(cartBox5);
-      cartBox1.appendChild(cartBoxRemove);
+    function openModal(message) {
+      const alertModal = document.getElementById("myAlertModal");
+      const alertModalMessage = document.getElementById("alertModalMessage");
+      alertModalMessage.textContent = message;
+      alertModal.style.display = "block";
+    }
+
+    function closeModal() {
+      const alertModal = document.getElementById("myAlertModal");
+      alertModal.style.display = "none";
+    }
+
+    function handleAlertConfirm() {
+      const alertModalMessage =
+        document.getElementById("alertModalMessage").textContent;
+
+      if (alertModalMessage === "로그아웃하시겠습니까?") {
+        axios
+          .post(
+            "http://localhost:8080/user/logout",
+            {},
+            { withCredentials: true }
+          )
+          .then((response) => {
+            console.log("데이터: ", response);
+            if (response.status === 200) {
+              closeModal(); // 모달 닫기
+              document
+                .querySelector(".menuLoginBtn")
+                .classList.remove("hidden");
+              document.querySelector(".menuLogoutBtn").classList.add("hidden");
+            }
+          })
+          .catch((error) => {
+            console.log("에러 발생: ", error);
+          });
+      } else if (alertModalMessage === "장바구니에서 삭제하시겠습니까?") {
+        axios
+          .delete(
+            "http://localhost:8080/cart/delete/" +
+              userId +
+              "/" +
+              itemToDelete.id
+          )
+          .then((response) => {
+            console.log("데이터: ", response);
+            const cartBox1 = document.querySelector(
+              `[data-item-id="${itemToDelete.id}"]`
+            );
+            if (cartBox1) cartBox1.remove();
+            closeModal(); // 모달 닫기
+          })
+          .catch((error) => {
+            console.log("에러 발생:", error);
+          });
+      } else if (alertModalMessage === "구매하시겠습니까?") {
+        saveLecture(userId, cartItems);
+        closeModal(); // 모달 닫기
+        openModal("구매 완료! 마이페이지에서 확인할 수 있습니다.");
+      } else if (
+        alertModalMessage === "구매 완료! 마이페이지에서 확인할 수 있습니다."
+      ) {
+        closeModal();
+        window.location.reload();
+      }
+    }
+
+    document.querySelector(".menuLogoutBtn").addEventListener("click", () => {
+      openModal("로그아웃하시겠습니까?");
     });
 
-    updateTotalPrice(item);
+    document.querySelector(".alertClose").addEventListener("click", closeModal);
   }
 
   // 총 가격을 업데이트하는 함수
   function updateTotalPrice(cartItems) {
     const totalPriceContainer = document.getElementById("totalPriceContainer");
-    const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0);
+    const totalPrice = cartItems.reduce(
+      (sum, item) => sum + item.lecture.price,
+      0
+    );
     totalPriceContainer.textContent = `총 가격: ${totalPrice.toLocaleString()}원`;
   }
-
-  // 구매 확인 모달 열기
-  document.querySelector(".cartBox6").addEventListener("click", () => {
-    openModal("구매하시겠습니까?");
-  });
-
-  function openModal(message) {
-    const alertModal = document.getElementById("myAlertModal");
-    const alertModalMessage = document.getElementById("alertModalMessage");
-    alertModalMessage.textContent = message;
-    alertModal.style.display = "block";
-  }
-
-  function closeModal() {
-    const alertModal = document.getElementById("myAlertModal");
-    alertModal.style.display = "none";
-  }
-
-  document.querySelector(".menuLogoutBtn").addEventListener("click", () => {
-    openModal("로그아웃하시겠습니까?");
-  });
-
-  // 모달 내 확인 버튼 클릭 시 로그아웃 처리 또는 장바구니 아이템 삭제 처리
-  document.getElementById("alertConfirm").addEventListener("click", () => {
-    const alertModalMessage = document.getElementById("alertModalMessage").textContent;
-
-    if (alertModalMessage === "로그아웃하시겠습니까?") {
-      axios
-        .post("http://localhost:8080/user/logout", {}, { withCredentials: true })
-        .then((response) => {
-          console.log("데이터: ", response);
-          if (response.status === 200) {
-            closeModal(); // 모달 닫기
-            alert("로그아웃 되었습니다");
-            document.querySelector(".menuLoginBtn").classList.remove("hidden");
-            document.querySelector(".menuLogoutBtn").classList.add("hidden");
-          }
-        })
-        .catch((error) => {
-          console.log("에러 발생: ", error);
-        });
-    } else if (alertModalMessage === "장바구니에서 삭제하시겠습니까?") {
-      itemToDelete.splice(itemIndexToDelete, 1);
-      fetchAndRenderCart(itemToDelete, userId);
-      closeModal(); // 모달 닫기
-    } else if (alertModalMessage === "구매하시겠습니까?") {
-      let cartItems = JSON.parse(localStorage.getItem(userId));
-      localStorage.setItem(userId + "_purchased", JSON.stringify(cartItems));
-      localStorage.removeItem(userId);
-      window.location.reload();
-      closeModal(); // 모달 닫기
-      openModal("구매 완료! 마이페이지에서 확인할 수 있습니다.");
-    }
-  });
-
-  // 모달 내 취소 버튼 클릭 시 모달 닫기
-  document.querySelector(".alertClose").addEventListener("click", closeModal);
-
 });
+
+function saveLecture(userId, cartItem) {
+  cartItem.forEach((item) => {
+    axios
+      .post(
+        "http://localhost:8080/edutech/save/" +
+          userId +
+          "/" +
+          item.lecture.lectureId
+      )
+      .then((response) => {
+        console.log("데이터: ", response);
+        axios
+          .delete("http://localhost:8080/cart/delete/" + userId + "/" + item.id)
+          .then((response) => {
+            console.log("데이터: ", response);
+            const cartBox1 = document.querySelector(
+              `[data-item-id="${item.id}"]`
+            );
+            if (cartBox1) cartBox1.remove();
+          })
+          .catch((error) => {
+            console.log("에러 발생:", error);
+          });
+      })
+      .catch((error) => {
+        console.log("에러 발생 : ", error);
+      });
+  });
+}
